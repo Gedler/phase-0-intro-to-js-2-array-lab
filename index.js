1b040cb36b88d5ced540d721defaf2a1a1edc7c6
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-03-09 09:07:42 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ['Milo', "Otis", "Garfield"]; 
+const Broom = ['Whatever','Luck','Needed'];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    return name;
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+    return name;
+}
+function destructivelyRemoveLastCat(name) { 
+    cats.pop(); 
+    return name;
+}
+function destructivelyRemoveFirstCat(name) { 
+    cats.shift();
+    return name;
+}
+function appendCat(name) {
+    return [...cats,name]
+    
+}
+function prependCat(name) {
+    return [name,...cats]
+}
+function removeLastCat() {
+    return cats.slice(0,cats.length-1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
+cats;
+Broom;
+destructivelyAppendCat;
+destructivelyPrependCat;
+destructivelyRemoveLastCat;
+appendCat;
+prependCat;
+removeLastCat;
+removeFirstCat;
